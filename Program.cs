ae7dd3d902bd15a548d9d76b8697e715e0cf2316
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-09-09 10:19:04 +0300

    Skriv ut kursutbud baserat på techniques sträng

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*
+ * Data ifrån API
+ */
+string[] techniques = { "C#", "Databaser", "Webb", "Clean Code" };
 
-string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>";
-string welcome = "<h1> Välkomna! </h1>";
-string kurser = "<p> Kurs om C#</p>\n<p>Kurs om Databaser</p>";
+/*
+ * Statisk data
+ */
+string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
+string welcome = "<h1> Välkomna! </h1>\n";
+string kurser = "";
+
+foreach (string technique in techniques)
+{
+    kurser += "<p>" + technique + "</p>\n";
+}
+
 string end = "</main>\n</body>\n</html>";
 
-Console.WriteLine($"{start}\n{welcome}\n{kurser}\n{end}");
+/*
+ * Skriva ut data
+ */
+
+Console.WriteLine($"{start}{welcome}{kurser}{end}");
