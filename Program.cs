d86d572217beddd23ceae08eba2e909bd879d578
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-09-09 11:14:47 +0300

    First OOP version

--- Program.cs
+++ Program.cs
@@ -1,64 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 /*
- * Data ifrån API
- */
-string[] techniques = {"   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   "};
-string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
-
-/*
  * Skriva ut data
  */
-printPage();
+WebsiteGenerator website = new WebsiteGenerator();
 
-void printStart()
+website.printPage();
+
+
+class WebsiteGenerator
 {
-    string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
-    Console.WriteLine(start);
-}
-void printWelcome(string className, string[] message)
-{
-    string welcome = $"<h1> Välkomna {className}! </h1>";
+    /*
+     * Data ifrån API
+     */
+    public string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
+    public string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
 
-    string welcomeMessage = "";
-
-    foreach (string msg in message)
-    {
-        welcomeMessage += $"\n<p><b> Meddelande: </b> {msg} </p>";
-    }
-    
-    Console.WriteLine(welcome + welcomeMessage);
-}
-void printKurser()
-{
-    string kurser = courseGenerator(techniques);
-    Console.WriteLine(kurser);
-}
-void printEnd()
-{
-    string end = "</main>\n</body>\n</html>";
-    Console.WriteLine(end);
-}
-
-
-void printPage()
-{
-    printStart();
-    printWelcome("Klass A", messagesToClass);
-    printKurser();
-    printEnd(); 
-}
-
-
-string courseGenerator(string[] techniques)
-{
+    string className = "Klass A";
     string kurser = "";
 
-    foreach (string technique in techniques)
+    public void printStart()
     {
-        string tmp = technique.Trim();
-        kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+        string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
+        Console.WriteLine(start);
+    }
+    public void printWelcome(string className, string[] message)
+    {
+        string welcome = $"<h1> Välkomna {className}! </h1>";
+
+        string welcomeMessage = "";
+
+        foreach (string msg in message)
+        {
+            welcomeMessage += $"\n<p><b> Meddelande: </b> {msg} </p>";
+        }
+
+        Console.WriteLine(welcome + welcomeMessage);
+    }
+    public void printKurser()
+    {
+        string kurser = courseGenerator(this.techniques);
+        Console.WriteLine(kurser);
+    }
+    public void printEnd()
+    {
+        string end = "</main>\n</body>\n</html>";
+        Console.WriteLine(end);
     }
 
-    return kurser;
+    public void printPage()
+    {
+        printStart();
+        printWelcome(this.className, this.messagesToClass);
+        printKurser();
+        printEnd();
+    }
+    public string courseGenerator(string[] techniques)
+    {
+
+        foreach (string technique in techniques)
+        {
+            string tmp = technique.Trim();
+            kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+        }
+
+        return kurser;
+    }
 }
