08a694dc59c0ce938f7756fed4de3a1d3ae48d98
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-09-09 10:11:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>";
+string welcome = "<h1> Välkomna! </h1>";
+string kurser = "<p> Kurs om C#</p>\n<p>Kurs om Databaser</p>";
+string end = "</main>\n</body>\n</html>";
+
+Console.WriteLine($"{start}\n{welcome}\n{kurser}\n{end}");
