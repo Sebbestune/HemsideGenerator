db84c01dfe65de5ede53894203980245a657930a
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-09-16 11:33:38 +0300

    Förklarande kommentarer

--- Program.cs
+++ Program.cs
@@ -1,37 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/*
- * Skriva ut data
- */
 
+
+/*
+ * De olika inputs som vi vill skicka in i objekten (hemsidorna)
+ */
 string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
 string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
 
+// Vi skapar ett objekt för att kunna hantera en hemsida
 WebsiteGenerator website = new WebsiteGenerator("Klass A", messagesToClass, techniques);
 
+// Vi skapar en hemsida som tillåter styling, vi skickar in en färg utöver andra delar
 StyledWebsiteGenerator styledWebsite = new StyledWebsiteGenerator("Klass A", "blue", messagesToClass, techniques);
 
+// Vi skriver ut våra hemsidor först vanliga och sedan stylade
 website.printPage();
 Console.WriteLine("-----------------------");
 styledWebsite.printPage();
 
 
+/*
+ * Vi skapar ett interface (ett "kontrakt") med de delar som vår klass måste innehålla
+ */
 interface Website
 {
     void printPage();
 }
 
-
+/*
+ * Vi skapar vår WebsiteGenerator klass, med denna kan vi skapa objekt senare
+ * Klassen innehåller data och behavior 
+ */
 class WebsiteGenerator : Website
 {
-    /*
-     * Data ifrån API
-     */
 
+    /*
+     * De olika egenskaperna (datat) i varje objekt
+     */
     string[] messagesToClass, techniques;
     string className;
     string kurser = "";
 
+    /*
+     * En konstruktor som tillåter oss att lägga in egen data i objektens egenskaper
+     */
     public WebsiteGenerator(string className, string[] messageToClass, string[] techniques)
     {
         this.className = className;
@@ -39,6 +52,10 @@ class WebsiteGenerator : Website
         this.techniques = techniques;
     }
 
+    /*
+     * Flera olika metoder för att utföra diverse funktionalitet
+     * virtual = tillåter oss att override:a (göra egen version utav) metoden i ärvda klasser
+     */
     virtual protected void printStart()
     {
         string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
@@ -75,6 +92,10 @@ class WebsiteGenerator : Website
         printKurser();
         printEnd();
     }
+
+    /*
+     * En utility metod
+     */
     string courseGenerator(string[] techniques)
     {
 
@@ -88,19 +109,30 @@ class WebsiteGenerator : Website
     }
 }
 
+/*
+ * Här ärver vi egenskaper och metoder ifrån WebsiteGenerator för att kunna återanvända delar i vår StyledWebsiteGenerator
+ */
 class StyledWebsiteGenerator : WebsiteGenerator
 {
-
+    // En extra egenskap
     string color;
 
+    /*
+     * En utökad konstruktor.
+     * Vi vill lägga in alla del egenskaper som behövs i base-klassen vi ärvde ifrån
+     * Och också lägga in en färg (data) i vår nya egenskap
+     */
     public StyledWebsiteGenerator(string className, string color, string[] messageToClass, string[] techniques) : base(className, messageToClass, techniques)
     {
         this.color = color;
     }
 
+    /*
+     * Vi skapar en egen version av printStart (override:ar den) för att kunna få resultatet vi önskar
+     */
     override protected void printStart()
     {
-        Console.WriteLine($"<!DOCTYPE html>\n<html>\n<head>\n<styles>\np {{ color: {this.color}; }}\n"+
+        Console.WriteLine($"<!DOCTYPE html>\n<html>\n<head>\n<styles>\np {{ color: {this.color}; }}\n" +
                           "</styles>\n</head>\n<body>\n<main>\n");
     }
 }
